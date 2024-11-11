50b9ead2413cc58c0a44a5adb345cc6f0284ba1f
Salal Saleem <salalsaleem2420@gmail.com>
2024-11-11 19:07:00 +0500

    updated some features of calculator

--- app.js
+++ app.js
@@ -5,54 +5,30 @@ function appendToScreen(param){
     screen.value += param;
 }
 
-function appendToPlus(param){
-    if(screen.value.at(-1) == "+"){
-        document.getElementById("plus").disbaled = true; 
+function appendToOperator(param){
+    if(screen.value.at(-1) == "+" || screen.value.at(-1) == "-" || screen.value.at(-1) == "/" || screen.value.at(-1) == "*" ){
+        screen.value = screen.value.slice(0, -1)+param;
         }
     else{
         screen.value += param;
     }
 }
-function appendToMinus(param){
-    if(screen.value.at(-1) == "-"){
-        document.getElementById("minus").disbaled = true; 
-    }
-    else{
-        screen.value += param;
-    }
-}
-function appendToMultiply(param){
-    if(screen.value.at(-1) == "*"){
-        document.getElementById("multiply").disbale; 
-    }
-    else{
-        screen.value += param;
-    }
-}
-function appendToDivide(param){
-    if(screen.value.at(-1) == "/"){
-        document.getElementById("divide").disbaled = true; 
-    }
-    else{
-        screen.value += param;
-    }
-}
-
      
 function clearScreen(){
     screen.value = ""
 }
 
 function calculateResult(){
-    try{
-    screen.value = eval(screen.value)
-        
-}catch(error){
-    screen.value = "Syntex Error"
+    if(screen.value != ""){
+        try{
+            screen.value = eval(screen.value)
+                
+        }catch(error){
+            screen.value = "Syntex Error"
+        }
+    }
+    
 }
-
-}
-
 function clearOne(){
     screen.value = screen.value.slice(0,-1) 
 }
